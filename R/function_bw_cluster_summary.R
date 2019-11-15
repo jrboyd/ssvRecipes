51644558d63eac79a348bbe0f4d969b97deea531
jrboyd <jrboyd@med.uvm.edu>
2019-11-15 09:41:39 -0500

    improved heatmap

--- R/function_bw_cluster_summary.R
+++ R/function_bw_cluster_summary.R
@@ -16,6 +16,39 @@ fetch_bw_summary = function(qdt, qgr){
     cor_mat
 }
 
+ggdend <- function(df) {
+    ggplot() +
+        geom_segment(data = df, aes(x=x, y=y, xend=xend, yend=yend)) +
+        labs(x = "", y = "") + theme_minimal() +
+        theme(axis.text = element_blank(), axis.ticks = element_blank(),
+              panel.grid = element_blank())
+}
+
+#' Title
+#'
+#' @param plot_list 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library(ssvRecipes)
+#' mat = matrix(runif(100), ncol = 10)
+#' plot_list = plot_hclust_heatmap(mat)
+#' plot_list$heatmap = plot_list$heatmap + scale_fill_viridis_c(limits = c(-1, 1))
+#' plot_hclust_heatmap.assemble(plot_list)
+plot_hclust_heatmap.assemble = function(plot_list){
+    p = plot_list$heatmap
+    if(!is.null(plot_list$x_dendrogram)){
+        p = cowplot::insert_xaxis_grob(p, plot_list$x_dendrogram)        
+    }
+    if(!is.null(plot_list$y_dendrogram)){
+        p = cowplot::insert_yaxis_grob(p, plot_list$y_dendrogram)        
+    }
+    plot(p)
+    invisible(p)
+}
+
 #' Title
 #'
 #' @param x 
@@ -26,37 +59,59 @@ fetch_bw_summary = function(qdt, qgr){
 #' @examples
 #' library(ssvRecipes)
 #' mat = matrix(runif(100), ncol = 10)
-#' debug(plot_hclust_heatmap)
 #' plot_hclust_heatmap(mat)
+#' plot_hclust_heatmap(mat, Rowv = FALSE)
+#' plot_hclust_heatmap(mat, Colv = FALSE)
+#' plot_hclust_heatmap(mat, dendrogram = "none")
+#' plot_hclust_heatmap(mat, dendrogram = "row")
+#' plot_hclust_heatmap(mat, dendrogram = "column")
 #' 
 #' @import ggdendro
-plot_hclust_heatmap = function(x){
+plot_hclust_heatmap = function(x, 
+                               Rowv = TRUE, Colv = TRUE, 
+                               dendrogram = c("both", "row", "column", "none")[1]){
+    stopifnot(is.matrix(x))
+    stopifnot(dendrogram %in% c("both", "row", "column", "none"))
+    stopifnot(is.logical(Rowv))
+    stopifnot(is.logical(Colv))
+    px = NULL
+    py = NULL
+    if(!Rowv){
+        if(dendrogram == "both") dendrogram = "column"
+        if(dendrogram == "row") dendrogram = "none"
+    }
+    if(!Colv & dendrogram %in% c("both", "row")){
+        if(dendrogram == "both") dendrogram = "row"
+        if(dendrogram == "column") dendrogram = "none"
+    }
     if(is.null(colnames(x))){
         colnames(x) = paste0("col", seq_len(ncol(x)))
     }
     if(is.null(rownames(x))){
         rownames(x) = paste0("row", seq_len(nrow(x)))
     }
-    dd.col <- as.dendrogram(hclust(dist(x)))
-    dd.row <- as.dendrogram(hclust(dist(t(x))))
-    dx <- ggdendro::dendro_data(dd.row)
-    dy <- ggdendro::dendro_data(dd.col)
     
-    ggdend <- function(df) {
-        ggplot() +
-            geom_segment(data = df, aes(x=x, y=y, xend=xend, yend=yend)) +
-            labs(x = "", y = "") + theme_minimal() +
-            theme(axis.text = element_blank(), axis.ticks = element_blank(),
-                  panel.grid = element_blank())
+    if(Rowv){
+        dd.col <- as.dendrogram(hclust(dist(x)))
+        dy <- ggdendro::dendro_data(dd.col)
+        py <- ggdend(dy$segments) + coord_flip()
+        col.ord <- order.dendrogram(dd.col)    
+    }else{
+        col.ord = seq_len(ncol(x))
+    }
+    
+    if(Colv){
+        dd.row <- as.dendrogram(hclust(dist(t(x))))
+        dx <- ggdendro::dendro_data(dd.row)
+        px <- ggdend(dx$segments)
+        row.ord <- rev(order.dendrogram(dd.row))    
+    }else{
+        row.ord = seq_len(nrow(x))
     }
     
     # x/y dendograms
-    px <- ggdend(dx$segments)
-    py <- ggdend(dy$segments) + coord_flip()
     
     # heatmap
-    col.ord <- order.dendrogram(dd.col)
-    row.ord <- rev(order.dendrogram(dd.row))
     xx <- x[col.ord, row.ord]
     xx_names <- attr(xx, "dimnames")
     df <- as.data.frame(xx)
@@ -67,12 +122,23 @@ plot_hclust_heatmap = function(x){
     mdf$col_var = mdf$variable
     p <- ggplot(mdf, aes(x = col_var, y = row_var)) + geom_tile(aes(fill = value))
     p = p + 
-        coord_fixed() + 
+        # coord_fixed() + 
         #scale_fill_gradientn(colours = c("blue", "white", "red"), limits = c(-1, 1)) +
         labs(x = "", y = "", fill = "value") + 
         theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = .5, size = 6),
               axis.text.y = element_text(size = 6),
               plot.subtitle = element_text(size = 8),
-              plot.title = element_text(size = 10)) 
-    p
+              plot.title = element_text(size = 10),
+              panel.background = element_blank(), 
+              panel.grid = element_blank()) 
+    gg_out = list(heatmap = p, x_dendrogram = px, y_dendrogram = py)
+    if(dendrogram %in% c("both", "row")){
+        p = cowplot::insert_yaxis_grob(p, py)    
+        
+    }
+    if(dendrogram %in% c("both", "column")){
+        p = cowplot::insert_xaxis_grob(p, px)    
+    }
+    plot(p)
+    invisible(gg_out)
 }
